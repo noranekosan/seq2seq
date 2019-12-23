edc576f3f24eac0bbda612fc1482357fc42c593b
noranekosan <bhas1270394@gn.iwasaki.ac.jp>
2019-12-23 10:14:28 +0900

    add feature

--- seq2seq/dlfs2/ch07/generate_better_text.py
+++ seq2seq/dlfs2/ch07/generate_better_text.py
@@ -10,7 +10,7 @@ corpus, word_to_id, id_to_word = ptb.load_data('train')
 vocab_size = len(word_to_id)
 corpus_size = len(corpus)
 
-
+  
 model = BetterRnnlmGen()
 model.load_params('../ch04/vocab.pkl')
 
@@ -19,7 +19,7 @@ start_word = '私'
 start_id = word_to_id[start_word]
 skip_words = ['N', '<unk>', '$']
 skip_ids = [word_to_id[w] for w in skip_words]
-# 文章生成 
+# 文章生成
 word_ids = model.generate(start_id, skip_ids)
 txt = ' '.join([id_to_word[i] for i in word_ids])
 txt = txt.replace(' <eos>', '.\n')
