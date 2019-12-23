1dd600b9731e7a6a58ab042666f9083f3ea6843a
noranekosan <bhas1270394@gn.iwasaki.ac.jp>
2019-12-23 10:21:10 +0900

    test

--- seq2seq/dlfs2/ch07/generate_better_text.py
+++ seq2seq/dlfs2/ch07/generate_better_text.py
@@ -10,12 +10,12 @@ corpus, word_to_id, id_to_word = ptb.load_data('train')
 vocab_size = len(word_to_id)
 corpus_size = len(corpus)
 
-   
+
 model = BetterRnnlmGen()
 model.load_params('../ch04/vocab.pkl')
 
 # start文字とskip文字の設定
-start_word = '私'
+start_word = 'わたし'
 start_id = word_to_id[start_word]
 skip_words = ['N', '<unk>', '$']
 skip_ids = [word_to_id[w] for w in skip_words]
