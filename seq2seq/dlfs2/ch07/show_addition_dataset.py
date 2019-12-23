e9e738cc8acb36f492a6bed0fe63f0a8db5a552d
noranekosan <bhas1270394@gn.iwasaki.ac.jp>
2019-12-23 10:33:11 +0900

    aaa

--- seq2seq/dlfs2/ch07/show_addition_dataset.py
+++ seq2seq/dlfs2/ch07/show_addition_dataset.py
@@ -11,7 +11,7 @@ word_to_id, id_to_char = sequence.get_vocab()
 print(x_train.shape, t_train.shape)
 print(x_test.shape, t_test.shape)
 # (45000, 7) (45000, 5)
-# (5000, 7) (5000, 5)
+# (5000, 7) (5000,  5)
 
 print(x_train[0])
 print(t_train[0])
